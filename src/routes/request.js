a31adf2d51f0ec6b31078ebfde714af6b092c946
Lobsang Mercedes <thecodesoflobs@gmail.com>
2021-06-25 10:46:10 -0400

    Rama test 1 request panel

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/request/newrequest', (req, res)=>{
+    res.render('request/new-request');
+
+});
+
+
 router.get('/users/notes', (req, res )=>{
     res.send('Panel de usuario');
 });
